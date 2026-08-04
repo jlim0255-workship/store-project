b40b7d1a89823e8819401f3903a20f8569a38b7f
jlim0255-workship <jlim0255.work@gmail.com>
2026-08-04 11:09:26 +0800

    created new test suites for GetProductByCode
    included an integration test (whole flow) & slice test (DataJpaTest for the repository method only)

--- catalog-service/src/test/java/com/jlim/store/catalogservice/domain/ProductRepositoryTest.java
+++ catalog-service/src/test/java/com/jlim/store/catalogservice/domain/ProductRepositoryTest.java
@@ -5,12 +5,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
 import org.springframework.test.context.jdbc.Sql;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 // MEAT: specifying this will do testing using in-memory database (h2) need to specify dependency before using
 // but we carry out the param here, spin up a postgres instance in a tc (reusable test container)
+// @DataJpaTest is slice annotation, we only testing a slice of a component instead of all controller -> service -> repository -> db flow
 @DataJpaTest(
         properties = {
                 "spring.test.database.replace=none",
@@ -28,5 +30,20 @@ class ProductRepositoryTest {
     void shouldGetAllProducts(){
         List<ProductEntity> products = productRepository.findAll();
         assertThat(products).hasSize(15);
+    }
+
+    @Test
+    void shouldGetProductByCode(){
+        ProductEntity product = productRepository.findByCode("P100").orElseThrow();
+        assertThat(product.getCode()).isEqualTo("P100");
+        assertThat(product.getName()).isEqualTo("The Hunger Games");
+        assertThat(product.getDescription()).isEqualTo("Winning will make you famous. Losing means certain death...");
+        assertThat(product.getPrice()).isEqualTo(new BigDecimal("34.0"));
+
+    }
+
+    @Test
+    void shouldReturnEmptyWhenProductCodeNotExists() {
+        assertThat(productRepository.findByCode("invalid_product_code")).isEmpty();
     }
 }
