ef00590ed2aa0ed9c8840cc4884a16fde80d6ae3
jlim0255-workship <jlim0255.work@gmail.com>
2026-08-04 11:08:31 +0800

    misc: added comments

--- catalog-service/src/test/java/com/jlim/store/catalogservice/AbstractIT.java
+++ catalog-service/src/test/java/com/jlim/store/catalogservice/AbstractIT.java
@@ -8,6 +8,9 @@ import org.springframework.context.annotation.Import;
 
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+// MEAT: the SpringBootTest.WebEnvironment.RANDOM_PORT will start the application on a random port, (test the whole flow)
+// and the @LocalServerPort annotation will inject that port into the test class.
+// This allows us to test the application in a real environment without hardcoding the port number.
 public abstract class AbstractIT {
     @LocalServerPort
     int port;
