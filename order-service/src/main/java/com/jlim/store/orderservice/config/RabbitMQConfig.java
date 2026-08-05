3419ff30d5a831d3a9ca8c83281ec00c6660722b
jlim0255-workship <jlim0255.work@gmail.com>
2026-08-05 13:43:25 +0800

    added rabbitmq exchange and queue

---
+++ order-service/src/main/java/com/jlim/store/orderservice/config/RabbitMQConfig.java
@@ -0,0 +1,87 @@
+package com.jlim.store.orderservice.config;
+
+import com.jlim.store.orderservice.ApplicationProperties;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.QueueBuilder;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import tools.jackson.databind.json.JsonMapper;
+
+@Configuration
+class RabbitMQConfig {
+    private final ApplicationProperties properties;
+
+    // injection
+    RabbitMQConfig(ApplicationProperties properties) {
+        this.properties = properties;
+    }
+
+    // step 1: define exchange
+    @Bean
+    DirectExchange exchange() {
+        return new DirectExchange(properties.orderEventsExchange());
+    }
+
+    // step 2: define queue
+    @Bean
+    Queue newOrdersQueue() {
+        return QueueBuilder.durable(properties.newOrdersQueue()).build();
+    }
+
+    // step 3: bind queue with the exchange
+    @Bean
+    Binding newOrdersQueueBinding() {
+        return BindingBuilder.bind(newOrdersQueue()).to(exchange()).with(properties.newOrdersQueue());
+    }
+
+    @Bean
+    Queue deliveredOrdersQueue() {
+        return QueueBuilder.durable(properties.deliveredOrdersQueue()).build();
+    }
+
+    @Bean
+    Binding deliveredOrdersQueueBinding() {
+        return BindingBuilder.bind(deliveredOrdersQueue()).to(exchange()).with(properties.deliveredOrdersQueue());
+    }
+
+    @Bean
+    Queue cancelledOrdersQueue() {
+        return QueueBuilder.durable(properties.cancelledOrdersQueue()).build();
+    }
+
+    @Bean
+    Binding cancelledOrdersQueueBinding() {
+        return BindingBuilder.bind(cancelledOrdersQueue()).to(exchange()).with(properties.cancelledOrdersQueue());
+    }
+
+    @Bean
+    Queue errorOrdersQueue() {
+        return QueueBuilder.durable(properties.errorOrdersQueue()).build();
+    }
+
+    @Bean
+    Binding errorOrdersQueueBinding() {
+        return BindingBuilder.bind(errorOrdersQueue()).to(exchange()).with(properties.errorOrdersQueue());
+    }
+
+    // rabbit template for sending msg
+    // producer send a bean, we have to convert it from bean (jackson) to Json
+    // then send it to msg queue
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory, JsonMapper jsonMapper) {
+        final var rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(jacksonConverter(jsonMapper));
+        return rabbitTemplate;
+    }
+
+    @Bean
+    public JacksonJsonMessageConverter jacksonConverter(JsonMapper mapper) {
+        return new JacksonJsonMessageConverter(mapper);
+    }
+}
