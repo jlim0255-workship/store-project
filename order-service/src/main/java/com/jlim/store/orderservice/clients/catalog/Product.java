5cb6eed176a7ad7f9668d783ede9075fd1a6c2e7
jlim0255-workship <jlim0255.work@gmail.com>
2026-08-06 13:51:20 +0800

    set up DTO for Product

---
+++ order-service/src/main/java/com/jlim/store/orderservice/clients/catalog/Product.java
@@ -0,0 +1,10 @@
+package com.jlim.store.orderservice.clients.catalog;
+
+import java.math.BigDecimal;
+
+public record Product(
+        String code,
+        String name,
+        String description,
+        String imageUrl,
+        BigDecimal price) {}
