0c6a8a39d7fed8aae94622349a70bc411c315b84
jlim0255-workship <jlim0255.work@gmail.com>
2026-08-06 13:51:09 +0800

    set up catalog-service client inside order-service

---
+++ order-service/src/main/java/com/jlim/store/orderservice/clients/catalog/ProductServiceClient.java
@@ -0,0 +1,46 @@
+package com.jlim.store.orderservice.clients.catalog;
+
+import io.github.resilience4j.retry.annotation.Retry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClient;
+
+import java.util.Optional;
+
+@Component
+public class ProductServiceClient {
+    private static final Logger log = LoggerFactory.getLogger(ProductServiceClient.class);
+
+    private final RestClient restClient;
+
+    ProductServiceClient(RestClient restClient) {
+        this.restClient = restClient;
+    }
+
+    @Retry(name="catalog-service", fallbackMethod = "getProductByCodeFallback")
+    public Optional<Product> getProductByCode(String code){
+        // MEAT
+        // default retry is 3 times
+        // if we put try catch, we assume we will get a successful response,
+        // but if we get a timeout, we will not retry, so we will get an exception.
+        // So we need to let the exception propagate to the retry mechanism.
+        log.info("Fetching product for code: {}", code);
+
+        var product = restClient
+                .get()
+                .uri("/api/products/{code}", code)
+                .retrieve()
+                .body(Product.class);
+
+        return Optional.ofNullable(product);
+
+    }
+
+    // if we cannot get after all retries, fallback reject and return empty
+    // do not assume, always check with the business team what is the default fallback if order cannot fulfil
+    Optional<Product> getProductByCodeFallback(String code, Throwable t){
+        System.out.println("ProuctServiceClient.getProductByCodeFallback: code: "+ code + ", error: " + t.getMessage());
+        return Optional.empty();
+    }
+}
