63e6d7128e0dc8e2489357cc7541322ef6882510
jlim0255-workship <jlim0255.work@gmail.com>
2026-08-07 14:52:33 +0800

    added shedlock config

---
+++ order-service/src/main/java/com/jlim/store/orderservice/config/SchedulerConfig.java
@@ -0,0 +1,29 @@
+package com.jlim.store.orderservice.config;
+
+import javax.sql.DataSource;
+import net.javacrumbs.shedlock.core.LockProvider;
+import net.javacrumbs.shedlock.provider.jdbctemplate.JdbcTemplateLockProvider;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+/**
+ * MEAT
+ * Even if we start multiple instances of order service,
+ * it is going to ensure ONLY ONE instance is processing 1 job (We have create new order and process order 2 jobs)
+ * at any point of time
+ *
+ * */
+
+@Configuration
+public class SchedulerConfig {
+    @Bean
+    public LockProvider lockProvider(DataSource dataSource) {
+        return new JdbcTemplateLockProvider(
+                JdbcTemplateLockProvider.Configuration.builder()
+                        .withJdbcTemplate(new JdbcTemplate(dataSource))
+                        .usingDbTime() // Works on Postgres, MySQL, MariaDb, MS SQL, Oracle, DB2, HSQL and H2
+                        .build()
+        );
+    }
+}
