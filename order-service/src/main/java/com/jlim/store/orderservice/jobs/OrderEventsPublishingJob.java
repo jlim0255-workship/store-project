d031d41300087199327d0eb9b44c74ea404eb3b2
jlim0255-workship <jlim0255.work@gmail.com>
2026-08-07 14:52:22 +0800

    added scheduler lock tag for jobs to be locked

--- order-service/src/main/java/com/jlim/store/orderservice/jobs/OrderEventsPublishingJob.java
+++ order-service/src/main/java/com/jlim/store/orderservice/jobs/OrderEventsPublishingJob.java
@@ -3,6 +3,8 @@ package com.jlim.store.orderservice.jobs;
 import java.time.Instant;
 
 import com.jlim.store.orderservice.domain.OrderEventService;
+import net.javacrumbs.shedlock.core.LockAssert;
+import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -19,7 +21,9 @@ public class OrderEventsPublishingJob {
     }
 
     @Scheduled(cron = "${orders.publish.order-events-job-cron}")
+    @SchedulerLock(name = "publishOrderEvents")
     public void publishOrderEvents(){
+        LockAssert.assertLocked();
         log.info("Publishing Order Events at {}", Instant.now());
         orderEventService.publishOrderEvents();
     }
--- order-service/src/main/java/com/jlim/store/orderservice/jobs/OrderProcessingJob.java
+++ order-service/src/main/java/com/jlim/store/orderservice/jobs/OrderProcessingJob.java
@@ -3,6 +3,8 @@ package com.jlim.store.orderservice.jobs;
 import java.time.Instant;
 
 import com.jlim.store.orderservice.domain.OrderService;
+import net.javacrumbs.shedlock.core.LockAssert;
+import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -20,7 +22,9 @@ public class OrderProcessingJob {
     }
 
     @Scheduled(cron = "${orders.new-orders-job-cron}")
+    @SchedulerLock(name = "processNewOrders")
     public void processNewOrders(){
+        LockAssert.assertLocked();
         log.info("Processing New Orders at {}", Instant.now());
         orderService.processNewOrders();
     }
