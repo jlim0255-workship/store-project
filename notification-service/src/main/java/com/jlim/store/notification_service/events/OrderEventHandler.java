3f3b9e5449c03cdb6ea551ff606e99df06c5a984
jlim0255-workship <jlim0255.work@gmail.com>
2026-08-08 22:21:21 +0800

    notification service module initial setup

---
+++ notification-service/src/main/java/com/jlim/store/notification_service/events/OrderEventHandler.java
@@ -0,0 +1,7 @@
+package com.jlim.store.notification_service.events;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class OrderEventHandler {
+}
---
+++ notification-service/src/test/java/com/jlim/store/notification_service/NotificationServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package com.jlim.store.notification_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(TestcontainersConfiguration.class)
+class NotificationServiceApplicationTests extends AbstractIT {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
