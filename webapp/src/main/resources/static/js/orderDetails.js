6065dfb3ab26208c425fcca32e3f572f0e72fc15
jlim0255-workship <jlim0255.work@gmail.com>
2026-08-12 22:36:50 +0800

    added order details page and its functionality

---
+++ webapp/src/main/resources/static/js/orderDetails.js
@@ -0,0 +1,21 @@
+document.addEventListener('alpine:init', () => {
+    Alpine.data('initData', (orderNumber) => ({
+        orderNumber: orderNumber,
+        orderDetails: {
+            items: [],
+            customer: {},
+            deliveryAddress: {}
+        },
+        init() {
+            updateCartItemCount();
+            this.getOrderDetails(this.orderNumber)
+        },
+        getOrderDetails(orderNumber) {
+            console.log(apiGatewayUrl)
+            $.getJSON(apiGatewayUrl + "/orders/api/orders/"+ orderNumber, (data) => {
+                //console.log("Get Order Resp:", data)
+                this.orderDetails = data
+            });
+        }
+    }))
+});
