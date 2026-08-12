a913274ba7c2e3e93481b6d62aa020a83a8698ef
jlim0255-workship <jlim0255.work@gmail.com>
2026-08-12 22:37:37 +0800

    fetch apigateway url

---
+++ webapp/src/main/java/com/jlim/store/webapp/ApplicationProperties.java
@@ -0,0 +1,7 @@
+package com.jlim.store.webapp;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix="store")
+public record ApplicationProperties (String apiGatewayUrl){
+}
