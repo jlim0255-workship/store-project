066fc2065d309c03fd24f9fcee7836cc75771fa8
jlim0255-workship <jlim0255.work@gmail.com>
2026-08-13 15:20:44 +0800

    added new annotation to pick up the application properties java class

--- webapp/src/main/java/com/jlim/store/webapp/WebappApplication.java
+++ webapp/src/main/java/com/jlim/store/webapp/WebappApplication.java
@@ -2,7 +2,9 @@ package com.jlim.store.webapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+@EnableConfigurationProperties(ApplicationProperties.class)
 @SpringBootApplication
 public class WebappApplication {
 
