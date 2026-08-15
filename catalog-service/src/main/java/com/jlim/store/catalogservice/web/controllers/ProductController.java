f4b65432b68da9d08aad6a00eeb875b8e19cb48a
jlim0255-workship <jlim0255.work@gmail.com>
2026-08-15 23:25:01 +0800

    added logs for grafana loki

--- catalog-service/src/main/java/com/jlim/store/catalogservice/web/controllers/ProductController.java
+++ catalog-service/src/main/java/com/jlim/store/catalogservice/web/controllers/ProductController.java
@@ -4,6 +4,8 @@ import com.jlim.store.catalogservice.domain.PagedResult;
 import com.jlim.store.catalogservice.domain.Product;
 import com.jlim.store.catalogservice.domain.ProductNotFoundException;
 import com.jlim.store.catalogservice.domain.ProductService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 class ProductController {
 
     private final ProductService productService;
+    private static final Logger log = LoggerFactory.getLogger(ProductController.class);
 
     ProductController(ProductService productService) {
         this.productService = productService;
@@ -20,11 +23,13 @@ class ProductController {
 
     @GetMapping
     PagedResult<Product> getProducts(@RequestParam(name = "page", defaultValue = "1") int pageNo) {
+        log.info("Fetching for page: {}", pageNo);
         return productService.getProducts(pageNo);
     }
 
     @GetMapping("/{code}")
     ResponseEntity<Product> getProductByCode(@PathVariable String code) {
+        log.info("Fetching product for code: {}", code);
         return productService
                 .getProductByCode(code)
                 .map(ResponseEntity::ok)
